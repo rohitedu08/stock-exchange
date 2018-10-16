c50f033f4a56e33f11ec995797c0c532f266c949
sharmar <rohit.sharma@rabobank.nl>
2018-10-16 18:05:55 +0200

    small fixes in the front-end

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,108 +1,112 @@
-var app = angular.module('app',[]);
+var app = angular.module('app', []);
 
-app.controller('StockCRUDCtrl', ['$scope','StockCRUDService', function ($scope,StockCRUDService) {
-	  
-	$scope.onloadFun = function() {
-		$scope.getAllStocks();
-      }
-	
-    $scope.updateStock = function (stock) {
-    	StockCRUDService.updateStock(stock)
-          .then(function success(response){
-        	  $scope.updateStockSuccess = 'Stock is updated!';
-        	  $scope.updateStockError='';
-              $scope.errorCode = '';
-          },
-          function error(response){
-        	  $scope.updateStockSuccess = '';
-        	  $scope.errorCode = response.data.errorCode;
-              $scope.updateStockError = response.data.message;
-          });
-    }
-    
-    $scope.getStock = function () {
-        var id = $scope.stock.id;
-        StockCRUDService.getStock($scope.stock.id)
-          .then(function success(response){
-              $scope.stock = response.data;
-              $scope.stock.id = id;
-              $scope.message='';
-              $scope.errorCode= '';
-          },
-          function error (response ){
-              $scope.message = response.data.message;
-              $scope.errorCode= response.data.errorCode;
-              
-          });
-    }
-    
-    $scope.addStock = function (stock) {
-        if (stock != null) {
-        	StockCRUDService.addStock(stock)
-              .then (function success(response){
-            	  $scope.stocks.push(response.data);
-                  $scope.addStockSuccess = 'Stock added!';
-                  $scope.addStockError='';
-                  $scope.errorMessage = '';
-              },
-              function error(response){
-            	  $scope.addStockSuccess = '';
-                  $scope.errorCode = response.data.errorCode
-                  $scope.addStockError = response.data.message;
-            });
-        }
-        else {
-            $scope.errorCode = 'Validation error';
-            $scope.message = 'Please enter a name and current price';
-            
-        }
-    }
-    
-    $scope.getAllStocks = function () {
-        StockCRUDService.getAllStocks()
-          .then(function success(response){
-              $scope.stocks = response.data;
-              $scope.message='';
-              $scope.errorCode = '';
-          },
-          function error (response ){
-              $scope.message='';
-              $scope.errorCode = 'Error getting stocks!';
-          });
-    }
+app.controller('StockCRUDCtrl', [
+		'$scope',
+		'StockCRUDService',
+		function($scope, StockCRUDService) {
 
-}]);
+			$scope.onloadFun = function() {
+				$scope.getAllStocks();
+				$scope.stateChanged = false;
+			}
 
-app.service('StockCRUDService',['$http', function ($http) {
-	
-    this.getStock = function getUser(userId){
-        return $http({
-          method: 'GET',
-          url: 'api/stocks/'+stockId
-        });
+			$scope.updateStock = function(stock) {
+				StockCRUDService.updateStock(stock).then(
+						function success(response) {
+							$scope.updateStockSuccess = 'Stock is updated!';
+							$scope.updateStockError = '';
+							$scope.errorCode = '';
+						}, function error(response) {
+							$scope.updateStockSuccess = '';
+							$scope.errorCode = response.data.errorCode;
+							$scope.updateStockError = response.data.message;
+						});
+
+			}
+
+			$scope.getStock = function() {
+				var id = $scope.stock.id;
+				StockCRUDService.getStock($scope.stock.id).then(
+						function success(response) {
+							$scope.stock = response.data;
+							$scope.stock.id = id;
+							$scope.message = '';
+							$scope.errorCode = '';
+						}, function error(response) {
+							$scope.message = response.data.message;
+							$scope.errorCode = response.data.errorCode;
+
+						});
+			}
+
+			$scope.addStock = function(stock) {
+				$scope.stateChanged = true;
+				if (stock != null && stock.name && stock.currentPrice) {
+					StockCRUDService.addStock(stock).then(
+							function success(response) {
+								$scope.stocks.push(response.data);
+								$scope.addStockSuccess = 'Stock added!';
+								$scope.addStockError = '';
+								$scope.errorMessage = '';
+							}, function error(response) {
+								$scope.addStockSuccess = '';
+								$scope.errorCode = response.data.errorCode
+								$scope.addStockError = response.data.message;
+							});
+				}
+
+			}
+
+			$scope.getAllStocks = function() {
+				StockCRUDService.getAllStocks().then(
+						function success(response) {
+							$scope.stocks = response.data;
+							$scope.message = '';
+							$scope.errorCode = '';
+						}, function error(response) {
+							$scope.message = '';
+							$scope.errorCode = 'Error getting stocks!';
+						});
+			}
+
+		} ]);
+
+app.service('StockCRUDService', [ '$http', function($http) {
+
+	this.getStock = function getUser(userId) {
+		return $http({
+			method : 'GET',
+			url : 'api/stocks/' + stockId
+		});
 	}
-	
-    this.addStock = function addUser(stock){
-        return $http({
-          method: 'POST',
-          url: 'api/stocks/',
-          data: {name:stock.name, currentPrice:stock.currentPrice}
-        });
-    }
-	
-    this.updateStock = function updateUser(stock){
-        return $http({
-          method: 'PUT',
-          url: 'api/stocks/'+stock.id,
-          data: {name:stock.name, currentPrice:stock.currentPrice}
-        })
-    }
-	
-    this.getAllStocks = function getAllStocks(){
-        return $http({
-          method: 'GET',
-          url: 'api/stocks/'
-        });
-    }
 
-}]);
+	this.addStock = function addUser(stock) {
+		return $http({
+			method : 'POST',
+			url : 'api/stocks/',
+			data : {
+				name : stock.name,
+				currentPrice : stock.currentPrice
+			}
+		});
+	}
+
+	this.updateStock = function updateUser(stock) {
+		return $http({
+			method : 'PUT',
+			url : 'api/stocks/' + stock.id,
+			data : {
+				name : stock.name,
+				currentPrice : stock.currentPrice
+			}
+		})
+	}
+
+	this.getAllStocks = function getAllStocks() {
+		return $http({
+			method : 'GET',
+			url : 'api/stocks/'
+		});
+	}
+
+} ]);
